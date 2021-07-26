67a8fd70a3f93db2cb91d51a1bf94a9f99b5a155
sdltsky <sedletskiyna@mail.ru>
2021-07-26 21:09:20 +0500

    закомментил основную активити

--- app/src/main/java/com/example/lessonone/MainActivity.java
+++ app/src/main/java/com/example/lessonone/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_activity);
+        //setContentView(R.layout.activity_main);
     }
 
     private void init(){
